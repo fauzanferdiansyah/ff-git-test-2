776245b11fee380a355552ec7f038d37e0ce23c4
Fauzan Ferdiansyah <ferdiansyah.fauzan@gmail.com>
2024-02-04 15:07:21 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus exercitationem officia beatae, molestiae, eveniet quam modi voluptatem dolorem quisquam tenetur non repellendus a necessitatibus saepe temporibus autem. Assumenda, obcaecati. Ullam!</p>
 </body>
 </html>
